30929065a06ee2bcf29dfd35e7eb3b5c7ff8dc74
Carmen Bruckmann <carmen.bruckmann@tq-group.com>
2024-05-30 17:50:20 +0200

    cmd/omitemptyremover/main.go: occuring errors are returned within the respective functions
    Signed-off-by: Carmen Bruckmann <carmen.bruckmann@tq-group.com>

--- cmd/omitemptyremover/main.go
+++ cmd/omitemptyremover/main.go
@@ -12,41 +12,47 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 	"os"
 	"strings"
+
+	"github.com/tq-systems/public-go-utils/log"
 )
 
 var dir = flag.String("d", "./", "directory")
 
-func main() {
+// run ensures that deferred calls are also executed in case of an error
+func run() error {
 	flag.Parse()
 
 	files, err := os.ReadDir(*dir)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to read directory: %v", err)
 	}
 
 	for _, f := range files {
 		info, err := f.Info()
 		if err != nil {
-			log.Fatal(err)
+			return fmt.Errorf("failed to get file info: %v", err)
 		}
-		removeOmitEmpty(info)
+		if err = removeOmitEmpty(info); err != nil {
+			return fmt.Errorf("failed removeOmitEmpty: %v", err)
+		}
 	}
+	return nil
 
 }
 
-func removeOmitEmpty(f os.FileInfo) {
+func removeOmitEmpty(f os.FileInfo) error {
 	if !f.IsDir() && strings.HasSuffix(f.Name(), ".pb.go") {
 		fileName := *dir + string(os.PathSeparator) + f.Name()
 		input, err := os.ReadFile(fileName)
 		if err != nil {
-			log.Panic(err)
+			return fmt.Errorf("failed reading file: %v", err)
 		}
 
 		allLines := strings.Split(string(input), "\n")
-		log.Println("Removing omitempty in: ", fileName)
+		log.Info("Removing omitempty in: ", fileName)
 		replacer := strings.NewReplacer(",omitempty", "")
 		for i, line := range allLines {
 			allLines[i] = replacer.Replace(line)
@@ -55,7 +61,18 @@ func removeOmitEmpty(f os.FileInfo) {
 		//READ, WRITE
 		err = os.WriteFile(fileName, []byte(output), 0666)
 		if err != nil {
-			log.Panic(err)
+			return fmt.Errorf("failed writing file: %v", err)
 		}
 	}
+
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Errorf("Failed to init app: %v", err)
+		os.Exit(1)
+	}
+
 }
