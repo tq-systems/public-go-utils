d7283da53549290fb965287832c9aa36cd4818b9
Kühnl Benno <Benno.Kuehnl@tq-group.com>
2026-04-23 07:47:34 +0200

    Update license headers
    Signed-off-by: Kühnl Benno <Benno.Kuehnl@tq-group.com>

--- cmd/omitemptyremover/main.go
+++ cmd/omitemptyremover/main.go
@@ -1,13 +1,16 @@
 /*
- * cmd/omitemptyremover - main.go
- * Copyright (c) 2018 - 2023 TQ-Systems GmbH <license@tq-group.com>, D-82229 Seefeld, Germany. All rights reserved.
- * Author: Marcel Matzat and the Energy Manager development team
+ * Copyright (c) 2023-2026 TQ-Systems GmbH <license@tq-group.com>, D-82229
+ * Seefeld, Germany. All rights reserved.
+ * Author: Christoph Krutz and the Energy Manager development team
  *
- * This software code contained herein is licensed under the terms and conditions of
- * the TQ-Systems Product Software License Agreement Version 1.0.1 or any later version.
- * You will find the corresponding license text in the LICENSE file.
+ * This software is licensed under the TQ-Systems Product Software License
+ * Agreement Version 1.0.3 or any later version.
+ * You can obtain a copy of the License Agreement in the TQS (TQ-Systems
+ * Software Licenses) folder on the following website:
+ * https://www.tq-group.com/en/support/downloads/tq-software-license-conditions/
  * In case of any license issues please contact license@tq-group.com.
  */
+
 package main
 
 import (
